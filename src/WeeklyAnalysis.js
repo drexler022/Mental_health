529e28bce4a4370dd02e83b16ab177a3dcdcfb43
drexler022 <111218836+drexler022@users.noreply.github.com>
2023-09-18 15:22:46 +1000

    Update WeeklyAnalysis.js

--- src/WeeklyAnalysis.js
+++ src/WeeklyAnalysis.js
@@ -4,8 +4,8 @@ import React, { useState } from 'react';
 const AWS = require('aws-sdk');
 
 AWS.config.update({
-  accessKeyId: 'AKIA44AM67MI4MF5F77C',
-  secretAccessKey: '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'
+  accessKeyId: '',
+  secretAccessKey: ''
 });
 
 function WeeklyAnalysis() {
